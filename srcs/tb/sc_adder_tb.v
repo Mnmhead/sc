2499f568cb23c95aa5162da2f0bfd4fb8fd8555d
Wyatt <gyorgym@cs.washington.edu>
2017-04-29 16:48:50 -0700

    Finished sc_adder testbench.

--- srcs/tb/sc_adder_tb.v
+++ srcs/tb/sc_adder_tb.v
@@ -14,62 +14,77 @@ module sc_adder_tb();
 
 	sc_adder dut( .x(x), .y(y), .sel(sel), .out(out) );
 
+    integer errors;
+    initial errors = 0;
 	initial begin
 		// initialize inputs
 		x = 0;
 		y = 0;
 		sel = 0;
-
-		#100;
+		#25;
 
 		// run through all combinations of inputs
-		x = 0; y = 0; sel = 0; 
+		x = 0; y = 0; sel = 0; #5;
 		if( out != 0 ) begin
 			$display("Error incorrect output. x: %d, y: %d, sel %d\n", x, y, sel);
+			errors = errors + 1;
 		end
-		#10;
+		#5;
 
-		x = 0; y = 0; sel = 1;
+		x = 0; y = 0; sel = 1; #5;
 		if( out != 0 ) begin
 			$display("Error incorrect output. x: %d, y: %d, sel %d\n", x, y, sel);
+			errors = errors + 1;
 		end
-		#10;
+		#5;
 
-		x = 0; y = 1; sel = 0;
+		x = 0; y = 1; sel = 0; #5;
 		if( out != 0 ) begin
 			$display("Error incorrect output. x: %d, y: %d, sel %d\n", x, y, sel);
+			errors = errors + 1;
 		end
-		#10;
+		#5;
 
-		x = 0; y = 1; sel = 1;
+		x = 0; y = 1; sel = 1; #5;
 		if( out != 1 ) begin
 			$display("Error incorrect output. x: %d, y: %d, sel %d\n", x, y, sel);
+			errors = errors + 1;
 		end
-		#10;
+		#5;
 
-		x = 1; y = 0; sel = 0;
+		x = 1; y = 0; sel = 0; #5;
 		if( out != 1 ) begin
 			$display("Error incorrect output. x: %d, y: %d, sel %d\n", x, y, sel);
+			errors = errors + 1;
 		end
-		#10;
+		#5;
 
-		x = 1; y = 0; sel = 1;
+		x = 1; y = 0; sel = 1; #5;
 		if( out != 0 ) begin
 			$display("Error incorrect output. x: %d, y: %d, sel %d\n", x, y, sel);
+			errors = errors + 1;
 		end
-		#10;
+		#5;
 
-		x = 1; y = 1; sel = 0;
+		x = 1; y = 1; sel = 0; #5;
 		if( out != 1 ) begin
 			$display("Error incorrect output. x: %d, y: %d, sel %d\n", x, y, sel);
+			errors = errors + 1;
 		end
-		#10;
+		#5;
 
-		x = 1; y = 1; sel = 1;
+		x = 1; y = 1; sel = 1; #5;
 		if( out != 1 ) begin
 			$display("Error incorrect output. x: %d, y: %d, sel %d\n", x, y, sel);
+			errors = errors + 1;
 		end
-		#10;
+		#5;
+		
+		if( errors > 0 ) begin
+		    $display("Validataion failure: %d error(s).", errors);
+		end else begin
+            $display("Validation successful.");
+        end
 
 		$stop;	
 	end
